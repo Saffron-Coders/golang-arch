8512b42a2ae278697f35b8057196a349442801f9
Dave Augustus <davetweetlive@gmail.com>
2021-05-04 17:30:51 +0530

    Creating a token

--- main.go
+++ main.go
@@ -90,3 +90,12 @@ func checkSig(msg, sig []byte) (bool, error) {
 	same := hmac.Equal(newSig, sig)
 	return same, nil
 }
+
+func createToken(c *UserClaims) (string, error) {
+	t := jwt.NewWithClaims(jwt.SigningMethodES512, c)
+	signedToken, err := t.SignedString(key)
+	if err != nil {
+		return "", fmt.Errorf("error in createToken while signing token: %w", err)
+	}
+	return signedToken, nil
+}
