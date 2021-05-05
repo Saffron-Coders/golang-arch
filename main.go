8b5f8a2d85c32cb80947c633013ca4fa621d7d00
Dave Augustus <davetweetlive@gmail.com>
2021-05-05 23:03:44 +0530

    session cookie hmac

--- main.go
+++ main.go
@@ -1,32 +1,57 @@
 package main
 
 import (
-	"encoding/base64"
-	"fmt"
+	"crypto/hmac"
+	"crypto/sha256"
+	"io"
+	"net/http"
 )
 
+func getCode(msg string) string {
+	hmac.New(sha256.New, []byte(""))
+}
+
 func main() {
-	msg := "This is totally fun get hands-on learning it from the ground up. Thank you for sharing this info with me and helping me"
-	encoded := encode(msg)
-	fmt.Println(encoded)
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/submit", bar)
 
-	s, err := decode(encoded)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(s)
+	http.ListenAndServe(":8080", nil)
 }
 
-func encode(msg string) string {
-	encoded := base64.URLEncoding.EncodeToString([]byte(msg))
-	return string(encoded)
-}
-
-func decode(encoded string) (string, error) {
-	s, err := base64.URLEncoding.DecodeString(encoded)
-	if err != nil {
-		return "", fmt.Errorf("Couldn't decode string: %w", err)
+func bar(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
 	}
 
-	return string(s), nil
+	email := r.FormValue("email")
+	if email == "" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	c := http.Cookie{
+		Name:  "session",
+		Value: "",
+	}
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	html := `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>HMAC Example</title>
+	</head>
+	<body>
+		<form action="/submit" method="POST">
+			<input type="text" name="email"/>
+			<input type="submit" />
+		</form>
+	</body>
+	</html>`
+
+	io.WriteString(w, html)
 }
