cada689d1326a5cbcd51d777e8269afb3a7c9dd6
Dave Augustus <davetweetlive@gmail.com>
2021-05-07 23:46:58 +0530

    Login method

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 
 	http.HandleFunc("/", index)
 	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
 
 	http.ListenAndServe(":8080", nil)
 }
@@ -19,7 +20,7 @@ func main() {
 var db = map[string][]byte{}
 
 func index(w http.ResponseWriter, r *http.Request) {
-	errMsg := r.FormValue("errormsg")
+	errMsg := r.FormValue("msg")
 	html := `<!DOCTYPE html>
 	<html>
 	<body>
@@ -32,10 +33,19 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 	  <label for="password">Password:</label><br>
 	  <input type="password" id="password" name="password" value=""><br><br>
-	  <input type="submit" value="Submit">
+	  <input type="submit" value="register">
+	</form> 
+	<br><br><br><br><br><br>
+	<form action="/login" method="POST">
+	  <label for="fname">Username:</label><br>
+	  <input type="text" id="username" name="username" ><br>
+
+	  <label for="password">Password:</label><br>
+	  <input type="password" id="password" name="password" value=""><br><br>
+	  <input type="submit" value="login">
 	</form> 
 	
-	<p>If you click the "Submit" button, the form-data will be sent to the db</p>
+	
 	<p> Error: %s </p>
 	</body>
 	</html>
@@ -46,32 +56,69 @@ func index(w http.ResponseWriter, r *http.Request) {
 func register(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
-		errorMsg := url.QueryEscape("Method isn't Post")
-		http.Redirect(w, r, "/?errormsg="+errorMsg, http.StatusSeeOther)
+		msg := url.QueryEscape("Method isn't Post")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
 		return
 	}
 
 	username := r.FormValue("username")
 	password := r.FormValue("password")
 	if username == "" {
-		errorMsg := url.QueryEscape("username is required")
-		http.Redirect(w, r, "/?errormsg="+errorMsg, http.StatusSeeOther)
+		msg := url.QueryEscape("username is required")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
 		return
 	}
 	if password == "" {
-		errorMsg := url.QueryEscape("password is required")
-		http.Redirect(w, r, "/?errormsg="+errorMsg, http.StatusSeeOther)
+		msg := url.QueryEscape("password is required")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
 		return
 	}
 
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		errorMsg := url.QueryEscape("internal server error")
-		http.Error(w, errorMsg, http.StatusInternalServerError)
+		msg := url.QueryEscape("internal server error")
+		http.Error(w, msg, http.StatusInternalServerError)
 		return
 	}
 
 	db[username] = hash
 	fmt.Println(db)
 	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		msg := url.QueryEscape("Method isn't Post")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	if username == "" {
+		msg := url.QueryEscape("username is required")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
+		return
+	}
+	if password == "" {
+		msg := url.QueryEscape("password is required")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	if _, ok := db[username]; !ok {
+		msg := url.QueryEscape("email password didn't match")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(db[username]), []byte(password))
+	if err != nil {
+		msg := url.QueryEscape("email password didn't match")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	msg := url.QueryEscape("Logged in as: " + username)
+	http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
 }
